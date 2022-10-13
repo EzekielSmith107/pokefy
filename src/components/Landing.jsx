a6b43cca1451846c8403214cf802f6cad3ac8fa1
EzekielSmith107 <ezesmith107@gmail.com>
2022-10-13 11:54:35 -0400

    working on landing page

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import test from '../images/test.png'
 
 const Landing = () => {
   return (
-    <div>Landing</div>
+    <div>
+      <h1>Pokefy</h1>
+      <p>Welcome to Pokefy! Here you can explore new music inspired by your favorite pokemon. Simply select a pokemon from down below and a song will be generated for you based on a variety of its characteristics. Have fun exploring!</p>
+      <h2>Choose your Pokemon!</h2>
+    </div>
   )
 }
 
