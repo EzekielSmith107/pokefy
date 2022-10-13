224ef29ed4e7a4ef122d4e0b5af23d81d1fc3ca4
EzekielSmith107 <ezesmith107@gmail.com>
2022-10-13 17:18:26 -0400

    working on routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
-import Landing from "./components/Landing";
-import Select from "./components/Select";
+import Landing from "./pages/Landing";
+import Song from "./pages/Song";
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <Landing />
-    </div>
+    <Routes>
+      <Route path="/" element={<Landing />}></Route>
+      <Route path="/song" element={<Song />}></Route>
+    </Routes>
   );
 }
 
