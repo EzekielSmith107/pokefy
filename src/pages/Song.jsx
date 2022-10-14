0219821c64f4cfafb069931982f3e4caf778cd4f
EzekielSmith107 <ezesmith107@gmail.com>
2022-10-14 16:52:03 -0400

    working on cors error

--- src/pages/Song.jsx
+++ src/pages/Song.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { React, useEffect, useState } from 'react'
+import axios from 'axios'
 import { useLocation } from 'react-router-dom'
 
 const Song = () => {
   const location = useLocation()
-  console.log(location)
+  let pokemon = location.state
+
+  function getSong(characteristic) {
+    axios.get(`https://api.deezer.com/search?q=track:"${characteristic}"`)
+      .then((response) => {
+        console.log(response)
+      })
+  }
+
+  useEffect(() => {
+    axios.get(`https://pokeapi.co/api/v2/pokemon-species/${pokemon}/`)
+      .then((response) => {
+        let characteristic = response.data.color.name
+        getSong(characteristic)
+      })
+  })
   
   return (
     <div>Song</div>
