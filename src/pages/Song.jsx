257e6531d821dd814040afe24f2fb40a834ad887
EzekielSmith107 <ezesmith107@gmail.com>
2022-10-20 14:24:44 -0400

    fixed audiobook filter

--- src/pages/Song.jsx
+++ src/pages/Song.jsx
@@ -11,7 +11,7 @@ const Song = () => {
 
   function createAttributeString(pokeObj) {
     let color = pokeObj.data.color.name;
-    let eggGroup = pokeObj.data.egg_groups[0].name;
+    let eggGroup = pokeObj.data.egg_groups[0].name.replace(/\d+/g, "");
 
     let attributeArray = [color, eggGroup];
 
@@ -51,19 +51,31 @@ const Song = () => {
   }, [getSong]);
 
   return (
-    <div>
-      <h1>Song</h1>
+    <div className="max-w-[1240px] mx-auto py-4 px-4 flex flex-col text-center justify-center">
+      <h1 className="py-4">Song</h1>
       <h3>{track[0]}</h3>
-      <img src={track[1]} alt="album cover"></img>
-      <audio controls src={track[2]} typeof="audio/mp3">
-        Browser does not support audio.
-      </audio>
-      <button onClick={() => navigate("/")}>Choose another pokemon!</button>
-      <button>
-        <a href={track[3]} target="blank">
-          See full song here!
-        </a>
-      </button>
+      <div className="flex text-center justify-center py-4">
+        <img className="w-auto h-auto" src={track[1]} alt="album cover"></img>
+      </div>
+      <div className="flex text-center justify-center">
+        <audio controls src={track[2]} typeof="audio/mp3">
+          Browser does not support audio.
+        </audio>
+      </div>
+
+      <div className="flex space-x-2 justify-center py-4">
+        <button
+          className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+          onClick={() => navigate("/")}
+        >
+          Choose another pokemon!
+        </button>
+        <button className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
+          <a href={track[3]} target="blank">
+            See full song here!
+          </a>
+        </button>
+      </div>
     </div>
   );
 };
